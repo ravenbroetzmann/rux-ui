d184f0b618232868e9b745ba12168aba7370db2a
ravenbroetzmann <ravenbroetzmann@mailbox.org>
2022-07-21 10:39:39 +0200

    aler inline

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Modal.scss';
+
+export interface ModalProps {}
+
+const Modal: React.FC<ModalProps> = ({}) => {
+  return <div></div>;
+};
+
+Modal.defaultProps = {};
+
+export default Modal;
+
